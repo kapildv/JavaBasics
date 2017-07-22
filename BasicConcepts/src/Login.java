990f6829542adde0cda3832c2d624f0a12f1d897
Kapil Dev <kdev@rdalabs.com>
2017-07-22 21:19:15 +0530

    printed class name and hello world

--- BasicConcepts/src/Login.java
+++ BasicConcepts/src/Login.java
@@ -1,10 +1,11 @@
 
 public class Login {
-	private static final String TAG= Login.class.getName();
+    private static final String TAG = Login.class.getName();
 
-	public static void main(String[] args) {
-		System.out.println(TAG);
+    public static void main(String[] args) {
+        System.out.println(TAG);
+        System.out.println("Hello World");
 
-	}
+    }
 
 }
