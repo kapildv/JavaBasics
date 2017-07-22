3cf69ed543955f06632c43152a9f04d785b0cbc4
Kapil Dev <kdev@rdalabs.com>
2017-07-22 23:14:54 +0530

    added the description of TAG variable

--- BasicConcepts/src/Login.java
+++ BasicConcepts/src/Login.java
@@ -1,5 +1,6 @@
 
 public class Login {
+    /*Too print the class name while using the debugging */
     private static final String TAG = Login.class.getName();
 
     public static void main(String[] args) {
