e371e8013bce0ea3a2561d00f3c0c8b8a5127f8c
Kapil <kdev@rdalabs.com>
2018-07-09 16:56:20 +0530

    add thread join example

---
+++ BasicConcepts/src/multithreading/joinexample/ThreadJoinExample.java
@@ -0,0 +1,25 @@
+package multithreading.joinexample;
+
+public class ThreadJoinExample {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyRunnable(), "t1");
+        Thread t2 = new Thread(new MyRunnable(), "t2");
+        Thread t3 = new Thread(new MyRunnable(), "t3");
+
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t3.start();
+
+    }
+}
